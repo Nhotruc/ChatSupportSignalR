ce05401be8016ae70b860c4bc9d59ab103dd42f1
YOUR NAME <YOUR EMAIL ADDRESS>
2019-07-03 14:06:30 +0700

    First Version

---
+++ ChatSupport/Scripts/chatAdmin.js
@@ -0,0 +1,89 @@
+﻿$(function () {
+    function connectSignalR(auth) {
+        var chat = $.connection.chatHub;
+        chat.client.receive = function (connectId, message) {
+            console.log('receive is call')
+            if (!userIds.hasOwnProperty(connectId)) {
+                userIds[connectId] = [{ type: 'client', message: message }];
+                $(".inbox_chat").append('<div data-customer-id="' + connectId + '" class="chat_list"> ' +
+                    '  <div class="chat_people"> ' +
+                    '      <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div> ' +
+                    '    <div class="chat_ib"> ' +
+                    '        <h5>LinhDepTrai <span class="chat_date">1/7/2019</span></h5> ' +
+                    '       <p> ' +
+                          message +
+                    '       </p>' +
+                    '    </div> ' +
+                    '  </div>' +
+                    '  </div>')
+            } else {
+                userIds[connectId].push({ type: 'client', message: message });
+                $('div[data-customer-id="' + connectId + '"] p').text(message);
+            }
+        }
+
+        chat.client.remove = function (connectId) {
+            $('div[data-customer-id="' + connectId + '"]').remove();
+            delete userIds[connectId];
+        }
+
+        chat.client.removeCustomer = function (connectId) {
+
+        }
+
+        $.connection.hub.qs = { "access_token": auth };
+
+        $.connection.hub.start().done(function () {
+            console.log("ket noi xong")
+        });
+    }
+    function authenticateUser(credentials) {
+        var body = {
+            grant_type: 'password',
+            username: credentials.username,
+            password: credentials.password
+        };
+
+        $.ajax({
+            url: 'http://localhost:60904/token',
+            type: 'POST',
+            dataType: 'json',
+            contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+            /* data: JSON.stringify(body), /* wrong */
+            data: body, /* right */
+            complete: function (result) {
+
+            },
+
+            success: function (result) {
+                console.log("success")
+                if (result == null || !result.hasOwnProperty('access_token')) {
+                    $("#txtEmailMess").text("Đăng nhập thất bại");
+                    return;
+                }
+
+                $('#myModal').modal('hide')
+                auth = result['access_token'];
+                connectSignalR(auth);
+
+            },
+
+            error: function (result) {
+                $("#txtEmailMess").text("Đăng nhập thất bại")
+            },
+        });
+        // session.isAuthenticated(true);
+        return true;
+    }
+    $('#myModal').modal({
+        backdrop: 'static',
+        keyboard: false  // to prevent closing with Esc button (if you want this too)
+    })
+    $("#myModal").modal('show');
+
+    $("#formLogin").submit(function (e) {
+        e.preventDefault();
+        authenticateUser({ username: $('#txtEmail').val(), password: $('#txtPassword').val() });
+    })
+
+})
---
+++ ChatSupport/signalr/hubs/ChatHub.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR.Hubs;
+using System.Diagnostics;
+
+namespace ChatSupport.signalr.hubs
+{
+    public class ChatHub : Hub
+    {
+        private static string adminId="";
+
+        private HashSet<string> customerIds = new HashSet<string>();
+        public void sendToAdmin(string name,string message)
+        {
+            Debug.WriteLine("sendToAdmin is call");
+            Debug.WriteLine("Adminid " + adminId);
+            if (string.IsNullOrEmpty(adminId))
+                Debug.WriteLine("ADmim id bi null");
+            else
+            {
+                Debug.WriteLine("Admin id k null");
+            }
+            if (!string.IsNullOrEmpty(adminId))
+            {
+                Clients.Client(adminId).receive(Context.ConnectionId,message);
+            }
+        }
+
+        public void sendToCustomer(string connectId,string message)
+        {
+            Debug.WriteLine("sendToCustomer is call");
+            if (customerIds.Contains(connectId))
+            {
+                Clients.Client(connectId).receive(message);
+            } else
+            {
+
+            }
+        }
+
+        public override Task OnConnected()
+        {
+            string token = Context.QueryString["access_token"];
+            if (string.IsNullOrEmpty(token))
+            {
+                Debug.WriteLine("Customer is connect");
+                customerIds.Add(Context.ConnectionId);
+            } else
+            {
+                adminId = Context.ConnectionId;
+                Debug.WriteLine("Admin is connect "+adminId);
+            }
+            return base.OnConnected();
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            if (!string.IsNullOrEmpty(adminId))
+            {
+                Clients.Client(adminId).remove(Context.ConnectionId);
+            }
+            customerIds.Remove(Context.ConnectionId);
+            return base.OnDisconnected(stopCalled);
+        }
+
+        public override Task OnReconnected()
+        {
+            if (!customerIds.Contains(Context.ConnectionId))
+            {
+                customerIds.Add(Context.ConnectionId);
+            }
+
+            return base.OnReconnected();
+        }
+
+    }
+
+}
